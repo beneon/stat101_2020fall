b70cc0fb7e65ddfeb8341f355950050565863e24
junming zheng <lucidstudio@gmail.com>
2020-10-24 15:40:24 +0800

    c03 mod

--- c03/01_central_tendency_sum.R
+++ c03/01_central_tendency_sum.R
@@ -4,21 +4,29 @@ library(purrr)
 library(ggplot2)
 library(dplyr)
 
-balls = c(rep(1,5),rep(2,11),rep(3,18),rep(4,10),rep(5,4),rep(6,3),rep(7,3),rep(8,3),rep(9,3),rep(10,2),rep(11,2),rep(12,2),13,14,15)
+balls = c(
+  rep(1,5),
+  rep(2,11),
+  rep(3,18),rep(4,10),
+  rep(5,4),rep(6,3),
+  rep(7,3),rep(8,3),
+  rep(9,3),rep(10,2),
+  rep(11,2),rep(12,2),
+  13,14,15)
 hist(balls)
 
 ball_sample = function(n){
   # n: size of sample
-  box_of_ball = c(-3,-2,-1,0,1,2,3)
-  #box_of_ball = balls
+  #box_of_ball = c(-3,-2,-1,0,1,2,3)
+  box_of_ball = balls
   sample_rst = sample(box_of_ball,n,replace = TRUE)
   return(sum(sample_rst))
 }
 
 trials = 100000
-sample_size = 50
-reference_mean = 0
-#reference_mean = mean(balls)
+sample_size = 1000
+#reference_mean = 0
+reference_mean = mean(balls)
 
 trial_num = 1:trials
 sample_sizes = rep(sample_size,trials)
