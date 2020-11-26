1258382cb66ea92b0e21c58e8084a32fbe613c5d
Junming Zheng <beneon@qq.com>
2020-11-26 18:05:38 +0800

    线性回归的说明

---
+++ c10/01_generate_points.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(dplyr)
+sample_size = 25
+x = rnorm(sample_size,10,2)
+beta0=3
+beta1=2.5
+u = rnorm(sample_size,0,4.5)
+y=beta0+beta1*x+u
+df = data.frame(x=x,y=y)
+fit = lm(y~x,data=df)
+hat_beta0=fit$coefficients[1]
+hat_beta1=fit$coefficients[2]
+df = df %>% mutate(hat_y=hat_beta0+hat_beta1*x,mean_y=mean(y),hat_u=y-hat_y)
+df %>% ggplot(aes(x=x,y=y))+
+  geom_point()+
+  geom_hline(aes(yintercept=mean(y)))+
+  geom_vline(aes(xintercept=mean(x)))+
+  geom_smooth(method='lm')+
+  geom_segment(aes(xend=x,yend=hat_y))
